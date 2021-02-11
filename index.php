2ae3494d3db5d59a648446cd39c345bfb5991dd3
crypt0maniak <60582682+crypt0maniak@users.noreply.github.com>
2021-02-11 14:22:21 +0100

    Pass a Youtube url through POST and GET requests
    -Examples-
    GET request: index.php?v=https://www.youtube.com/watch?v=aqz-KE-bpKQ
    GET request: index.php?v=aqz-KE-bpKQ
    POST request: [fill in the form at index.php]

--- index.php
+++ index.php
@@ -21,9 +21,9 @@ if(!empty($youtubeURL) && !filter_var($youtubeURL, FILTER_VALIDATE_URL) === fals
         $videoTitle = $videoDownloadLink[0]['title']; 
         $videoQuality = $videoDownloadLink[1]['quality']; 
         $videoFormat = $videoDownloadLink[2]['format']; 
-        $videoFileName = strtolower(str_replace(' ', '_', $videoTitle)).'.mp4'; 
+        $videoFileName = strtolower(str_replace(' ', ' ', $videoTitle)).'.mp4'; 
         $downloadURL = $videoDownloadLink[0]['url']; 
-        $fileName = preg_replace('/[^A-Za-z0-9.\_\-]/', '', basename($videoFileName)); 
+        $fileName = preg_replace('/[^A-Za-z0-9.\_\-]/', ' ', basename($videoFileName)); 
         
         if(!empty($downloadURL)){ 
             //Header for force download 
@@ -42,6 +42,69 @@ if(!empty($youtubeURL) && !filter_var($youtubeURL, FILTER_VALIDATE_URL) === fals
         $statusMsg = "Video not found try another One";
         $msgClass = 'error';
     } 
+}else{ 
+    // error Massage
+    $statusMsg = "URL not valid please enter valid URL";
+        $msgClass = 'error'; 
+}
+} else if(isset($_GET['v'])){
+
+// Load download class 
+include_once 'functions.php'; 
+$handler = new Download(); 
+ 
+// video url of youtube
+if (substr( $_GET['v'], 0, 5 ) === "https") {
+	$youtubeURL = $_GET['v'];
+} else {
+	$ytid = $_GET['v'];
+	$youtubeURL = 'https://www.youtube.com/watch?v='.$ytid;
+}
+ 
+// if URL valid 
+if(!empty($youtubeURL) && !filter_var($youtubeURL, FILTER_VALIDATE_URL) === false){
+
+    $downloader = $handler->getDownloader($youtubeURL); 
+    
+    $downloader->setUrl($youtubeURL); 
+      
+    if($downloader->hasVideo()){ 
+        $videoDownloadLink = $downloader->getVideoDownloadLink();
+        $videoTitle = $videoDownloadLink[0]['title']; 
+        $videoQuality = $videoDownloadLink[1]['quality']; 
+        $videoFormat = $videoDownloadLink[2]['format']; 
+        $videoFileName = strtolower(str_replace(' ', ' ', $videoTitle)).'.mp4'; 
+        $downloadURL = $videoDownloadLink[0]['url']; 
+        $fileName = preg_replace('/[^A-Za-z0-9.\_\-]/', ' ', basename($videoFileName));
+		
+        if(!empty($downloadURL)){
+			if(isset($_SERVER['HTTP_RANGE'])) {
+				if(preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $_SERVER['HTTP_RANGE'], $matches)) {
+					$begin=intval($matches[0]);
+					if(!empty($matches[1])) {
+						$end=intval($matches[1]);
+					}
+				}
+			}
+            //Header for force download
+            header("Cache-Control: public");
+			header("Content-Type: video/mp4");
+			header("Accept-Ranges: bytes");
+			//header('Content-Length:'.($end-$begin));
+			header("Content-Disposition: inline; filename=$fileName");
+			//header("Content-Range: bytes $begin-$end/$size");
+			header("Content-Transfer-Encoding: binary");
+			header("Connection: close");
+			
+			// Read the file 
+			readfile($downloadURL);
+		} 
+		
+	}else{ 
+		// error Massage
+		$statusMsg = "Video not found try another One";
+		$msgClass = 'error';
+	} 
 }else{ 
     // error Massage
     $statusMsg = "URL not valid please enter valid URL";
